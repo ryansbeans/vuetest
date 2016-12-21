0c5f09f542d78748dbff13df45e5029e9d51b19c
ryansbeans <ryansbeans@ryansbeansdeMacBook-Pro.local>
2016-12-21 11:04:06 +0800

    vue
    玩玩vue试试

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+
+    <ul>
+      <li><router-link to="/first">点我跳转到第一页</router-link></li>
+      <li><router-link to="/second">点我跳转到第二页</router-link></li>
+    </ul>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script >
+//引入
+import firstcomponent from './component/firstcomponent.vue'
+import secondcomponent from './component/secondcomponent.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello Ryan!'
+    }
+  },
+  components: { firstcomponent,secondcomponent }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/component/firstcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="firstcomponent">
+    <h1>{{ msg2 }}</h1>
+    <a> powered by {{ author }} </a>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      msg2:"vue2.0",
+      author: "ryansbeans"
+    }
+  }
+}
+</script>
+
+<style>
+</style>
